a9ccdfb50b7a454c306950804582775fb422fc00
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-02 00:35:45 +0800

    added services card component

---
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,10 @@
+
+
+const ServiceCard = () => {
+    
+    return (
+        <h1>hello this is me services card </h1>
+    );
+};
+
+export default ServiceCard;
--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -18,7 +18,11 @@ console.log(services)
                 <h2 className="text-5xl">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
             </div>
-            
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                   services.map(service=>)
+                }
+            </div>
         </div>
     );
 };

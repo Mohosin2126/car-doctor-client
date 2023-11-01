e8af65ff87d9f53746311c8d524f5982a93bdbd7
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-01 23:35:39 +0800

    added logo in navbar and footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-
+import logo from "../../../assets/logo.svg"
 const Footer = () => {
     return (
         <footer className="footer p-10 bg-base-200 text-base-content">
             <div>
-                
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                <img src={logo} alt="" />
+                <p>Car Doctor Industries Ltd.<br />Providing reliable tech since 1992</p>
             </div>
             <div>
                 <span className="footer-title">Services</span>

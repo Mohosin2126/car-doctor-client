d2b099f50718bd2e74ec12331e05b38a19d28d3c
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-02 14:27:07 +0800

    added authstatechange

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,7 +1,7 @@
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
 export  const AuthContext=createContext()
 
@@ -12,8 +12,19 @@ const [user,setUser]=useState(null)
 const [loading,setLoading]=useState(true)
 
 const createUser=(email,password)=>{
+    setLoading(true)
     return createUserWithEmailAndPassword(auth, email, password)
 }
+ 
+useEffect(()=>{
+   const unsubscribe= onAuthStateChanged(auth,currentUser=>{
+        setUser(currentUser)
+      setLoading(false)
+    })
+    return ()=>{
+        return unsubscribe()
+    }
+},[])
 
 
 const authInfo={

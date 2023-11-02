b1bf9eb08990b239dc1458edc63751aba18ff3da
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-02 12:21:26 +0800

    added signup component

--- src/main.jsx
+++ src/main.jsx
@@ -26,8 +26,8 @@ const router = createBrowserRouter([
       },
       {
         path:"/signup",
-        element:<SignUp></SignUp>
-      }
+        element:<SignUp></SignUp>,
+      },
 
     ]
   },

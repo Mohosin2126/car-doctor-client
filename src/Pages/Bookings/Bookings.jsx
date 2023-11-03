33a7afbceec92afdee3ec69aa17cce098e8ab298
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-03 16:14:58 +0800

    data loaded

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -1,9 +1,23 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 
 const Bookings = () => {
+    const { user } = useContext(AuthContext);
+    const [bookings, setBookings] = useState([]); 
+    useEffect(() => {
+        fetch(`http://localhost:5000/bookings?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setBookings(data))
+    }, []);
+
+
+
+
+
     return (
         <div>
-            <h1>hello there this is me bookings </h1>
+            <h1>{bookings.length}</h1>
         </div>
     );
 };

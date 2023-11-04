20290bb26741d49856139dc864adce5212ced998
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-04 23:37:58 +0800

    done

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,7 +2,7 @@
 import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import axios from "axios";
+// import axios from "axios";
 
 export  const AuthContext=createContext()
 
